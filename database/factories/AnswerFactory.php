61007848fdeb1533b3c3db9e18755fb6a647246a
Eka Febryani Siskawandari <ekafebryanisiskawandari123@gmail.com>
2019-04-05 12:37:01 +0700

    fake answer

--- database/factories/AnswerFactory.php
+++ database/factories/AnswerFactory.php
@@ -4,10 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Answer::class, function (Faker $faker) {
     return [
-        'title' => rtrim($faker->sentence(rand(5, 10)), "."),
         'body' => $faker->paragraphs(rand(3, 7), true),
-        'views' => rand(0, 10),
-        //'answers_count' => rand(0, 10),
-        'votes' => rand(-3, 10),
+        'user_id' => App\User::pluck('id')->random(),
+        'votes_count' => rand(0, 5),
     ];
 });
