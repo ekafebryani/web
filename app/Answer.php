9cc04372c8fdde34ba4914244b51c8b89cf73b01
Eka Febryani Siskawandari <ekafebryanisiskawandari123@gmail.com>
2019-04-12 11:41:25 +0700

    Update best_answer_id on questions table when answer removed in database layer

--- app/Answer.php
+++ app/Answer.php
@@ -32,17 +32,12 @@ class Answer extends Model
       });
 
       static::deleted(function ($answer){
-        $question = $answer->question;
-        $question->decrement('answers_count');
-        if ($question->best_answer_id == $answer->id) {
-            $question->best_answer_id = NULL;
-            $question->save();
-        }
+        $answer->question->decrement('answers_count');
       });
     }
 
     public function getStatusAttribute()
     {
-      return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
+      return $this->id == $this->question->best_answers_id ? 'vote-accepted' : '';
     }
   }
